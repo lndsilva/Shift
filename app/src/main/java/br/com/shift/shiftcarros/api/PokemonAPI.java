043c3e435560523b15381682e084b06bfa5d32ba
laercio Silva <lndsilva@gmail.com>
2017-10-07 17:52:01 -0300

    Serviço para pesquisa do pokemon

--- app/src/main/java/br/com/shift/shiftcarros/api/PokemonAPI.java
+++ app/src/main/java/br/com/shift/shiftcarros/api/PokemonAPI.java
@@ -6,11 +6,19 @@ import br.com.shift.shiftcarros.model.Pokemon;
 import br.com.shift.shiftcarros.model.PokemonResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+import static android.R.attr.path;
 
 
 public interface PokemonAPI  {
 
     @GET("api/v2/pokemon")
     Call<PokemonResponse> getPokemon();
+
+    @GET("api/v2/pokemon/{numeroPokemon}")
+    Call<Pokemon> getPokemon(@Path(value = "numeroPokemon") int numeroPokemon);
+
+
 
 }
