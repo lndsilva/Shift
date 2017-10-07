293f8a5335e79512bdb6b4986dbf2f2e198ed85a
laercio Silva <lndsilva@gmail.com>
2017-10-07 14:43:25 -0300

    Initial commit

---
+++ app/src/main/java/br/com/shift/shiftcarros/adapter/TabAdapter.java
@@ -0,0 +1,24 @@
+package br.com.shift.shiftcarros.adapter;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+
+public class TabAdapter extends FragmentStatePagerAdapter {
+
+    public TabAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
