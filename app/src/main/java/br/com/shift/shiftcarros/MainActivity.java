671dc402e6c5e9bfb74a8f2df4d265e02c8419b6
laercio Silva <lndsilva@gmail.com>
2017-10-07 15:09:55 -0300

    Criação do framelayout

--- app/src/main/java/br/com/shift/shiftcarros/MainActivity.java
+++ app/src/main/java/br/com/shift/shiftcarros/MainActivity.java
@@ -3,6 +3,8 @@ package br.com.shift.shiftcarros;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +14,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import br.com.shift.shiftcarros.adapter.TabAdapter;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +44,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        initTabLayout();
     }
 
     @Override
@@ -57,6 +63,8 @@ public class MainActivity extends AppCompatActivity
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+
+
     }
 
     @Override
@@ -81,10 +89,40 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
 
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void initTabLayout() {
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Pokemons"));
+        tabLayout.addTab(tabLayout.newTab().setText("Star Wars"));
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        TabAdapter tabAdapter = new TabAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(tabAdapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
--- app/src/main/java/br/com/shift/shiftcarros/adapter/TabAdapter.java
+++ app/src/main/java/br/com/shift/shiftcarros/adapter/TabAdapter.java
@@ -5,8 +5,14 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
+import br.com.shift.shiftcarros.fragments.PokemonFragment;
+import br.com.shift.shiftcarros.fragments.StarWarsFragment;
+
 
 public class TabAdapter extends FragmentStatePagerAdapter {
+
+    public static int TOTAL_ABAS = 2;
+
 
     public TabAdapter(FragmentManager fm) {
         super(fm);
@@ -14,11 +20,23 @@ public class TabAdapter extends FragmentStatePagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+        Fragment fragment = null;
+
+        switch (position) {
+            case 0:
+                fragment = new PokemonFragment();
+                break;
+            case 1:
+                fragment = new StarWarsFragment();
+
+                break;
+        }
+
+        return fragment;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return TOTAL_ABAS;
     }
 }
