c066faeefad0eb6364d7af2ecc38397656928f24
laercio Silva <lndsilva@gmail.com>
2017-10-07 16:35:31 -0300

    Criação do RecycleView

--- app/src/main/java/br/com/shift/shiftcarros/adapter/PokemonAdapter.java
+++ app/src/main/java/br/com/shift/shiftcarros/adapter/PokemonAdapter.java
@@ -1,17 +1,36 @@
 package br.com.shift.shiftcarros.adapter;
 
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.List;
+
+import br.com.shift.shiftcarros.R;
+import br.com.shift.shiftcarros.model.Pokemon;
+
 public class PokemonAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
+    private Context context;
+    private  List<Pokemon> pokemons;
+
+    public PokemonAdapter(Context context, List<Pokemon> pokemons){
+        this.context = context;
+        this.pokemons = pokemons;
+
+    }
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+     //estamos selecionando o layout da linha da nossa lista
+
+        View view = LayoutInflater.from(context).inflate(R.layout.pokemon_item, parent,false);
+
+        return new PokemonViewHolder(view);
     }
 
     @Override
@@ -21,6 +40,7 @@ public class PokemonAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
 
     @Override
     public int getItemCount() {
+
         return 0;
     }
 
@@ -29,10 +49,13 @@ public class PokemonAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
 
         TextView tvNomePokemon;
         ImageView ivPokemon;
-        
+
 
         public PokemonViewHolder(View itemView) {
             super(itemView);
+
+            tvNomePokemon = (TextView) itemView.findViewById(R.id.tvNomePokemon);
+            ivPokemon = (ImageView) itemView.findViewById(R.id.ivPokemon);
         }
     }
 }
